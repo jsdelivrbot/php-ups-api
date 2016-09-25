d11b19302bef4a37aa58e6d6bf8725dc0ceee11e
xujiajun <me@xujiajun.cn>
2016-09-25 15:30:17 +0800

    change address validation

--- src/AddressValidation.php
+++ src/AddressValidation.php
@@ -7,15 +7,14 @@ use Exception;
 use Psr\Log\LoggerInterface;
 use SimpleXMLElement;
 use stdClass;
-use Ups\Entity\Address;
-use Ups\Entity\AddressValidationResponse;
+use Ups\Entity\AddressNew;
 
 /**
  * Address Validation API Wrapper.
  */
-class AddressValidation extends Ups
+class AddressValidationNew extends Ups
 {
-    const ENDPOINT = '/XAV';
+    const ENDPOINT = '/AV';
 
     /**
      * @var RequestInterface
@@ -28,32 +27,14 @@ class AddressValidation extends Ups
      * @todo make private
      */
     public $response;
-
     /**
-     * @var int
+     * @var AddressNew
      */
-    private $requestOption;
-
-    /**
-     * @var Address
-     */
-    private $address;
-
-    /**
-     * @var int
-     */
-    private $maxSuggestion;
-    /**
-     * @var bool
-     */
-    private $useAVResponseObject = false;
+    private $addressNew;
 
     /**
      * Request Options.
      */
-    const REQUEST_OPTION_ADDRESS_VALIDATION = 1;
-    const REQUEST_OPTION_ADDRESS_CLASSIFICATION = 2;
-    const REQUEST_OPTION_ADDRESS_VALIDATION_AND_CLASSIFICATION = 3;
 
     /**
      * @param string|null $accessKey UPS License Access Key
@@ -70,53 +51,28 @@ class AddressValidation extends Ups
         $useIntegration = false,
         RequestInterface $request = null,
         LoggerInterface $logger = null
-    ) {
+    )
+    {
         if (null !== $request) {
             $this->setRequest($request);
         }
         parent::__construct($accessKey, $userId, $password, $useIntegration, $logger);
     }
 
-    /**
-     * Turn on returning of the AddressValidationResponse object
-     */
-    public function activateReturnObjectOnValidate()
+
+    /* Get address suggestions from UPS.
+    *
+    * @param $address
+    * @param int $requestOption
+    * @param int $maxSuggestion
+    *
+    * @throws Exception
+    *
+    * @return stdClass|AddressValidationResponse
+    */
+    public function validate($addressNew)
     {
-        $this->useAVResponseObject = true;
-    }
-
-    /**
-     * Turn off returning of the AddressValidationResponse object
-     */
-    public function deActivateReturnObjectOnValidate()
-    {
-        $this->useAVResponseObject = false;
-    }
-    /**
-     * Get address suggestions from UPS.
-     *
-     * @param $address
-     * @param int $requestOption
-     * @param int $maxSuggestion
-     *
-     * @throws Exception
-     *
-     * @return stdClass|AddressValidationResponse
-     */
-    public function validate($address, $requestOption = self::REQUEST_OPTION_ADDRESS_VALIDATION, $maxSuggestion = 15)
-    {
-        if ($maxSuggestion > 50) {
-            throw new \Exception('Maximum of 50 suggestions allowed');
-        }
-
-        if (!in_array($requestOption, range(1, 3))) {
-            throw new \Exception('Invalid request option supplied');
-        }
-
-        $this->address = $address;
-        $this->requestOption = $requestOption;
-        $this->maxSuggestion = $maxSuggestion;
-
+        $this->addressNew = $addressNew;
         $access = $this->createAccess();
         $request = $this->createRequest();
 
@@ -133,16 +89,12 @@ class AddressValidation extends Ups
                 (int)$response->Response->Error->ErrorCode
             );
         }
-        if ($this->useAVResponseObject) {
-            unset($response->Response);
-            $avResponse = new AddressValidationResponse($response, $requestOption);
-            return $avResponse;
-        }
-        return $this->formatResponse($response);
+
+        return json_decode(json_encode($response), TRUE);
     }
 
     /**
-     * Create the XAV request.
+     * Create the AV request.
      *
      * @return string
      */
@@ -159,47 +111,24 @@ class AddressValidation extends Ups
         $node = $xml->importNode($this->createTransactionNode(), true);
         $request->appendChild($node);
 
-        $request->appendChild($xml->createElement('RequestAction', 'XAV'));
+        $request->appendChild($xml->createElement('RequestAction', 'AV'));
 
-        if (null !== $this->requestOption) {
-            $request->appendChild($xml->createElement('RequestOption', $this->requestOption));
-        }
+        if (null !== $this->addressNew) {
+            $addressNode = $avRequest->appendChild($xml->createElement('Address'));
+            if ($this->addressNew->getCity()) {
+                $addressNode->appendChild($xml->createElement('City', $this->addressNew->getCity()));
+            }
+            if ($this->addressNew->getCountryCode()) {
+                $addressNode->appendChild($xml->createElement('CountryCode', $this->addressNew->getCountryCode()));
+            }
+            if ($this->addressNew->getStateProvinceCode()) {
+                $addressNode->appendChild($xml->createElement('StateProvinceCode', $this->addressNew->getStateProvinceCode()));
+            }
 
-        if (null !== $this->maxSuggestion) {
-            $avRequest->appendChild($xml->createElement('MaximumListSize', $this->maxSuggestion));
-        }
+            if ($this->addressNew->getPostalCode()) {
+                $addressNode->appendChild($xml->createElement('PostalCode', $this->addressNew->getPostalCode()));
+            }
 
-        if (null !== $this->address) {
-            $addressNode = $avRequest->appendChild($xml->createElement('AddressKeyFormat'));
-
-            if ($this->address->getAttentionName()) {
-                $addressNode->appendChild($xml->createElement('ConsigneeName', $this->address->getAttentionName()));
-            }
-            if ($this->address->getBuildingName()) {
-                $addressNode->appendChild($xml->createElement('BuildingName', $this->address->getBuildingName()));
-            }
-            if ($this->address->getAddressLine1()) {
-                $addressNode->appendChild($xml->createElement('AddressLine', $this->address->getAddressLine1()));
-            }
-            if ($this->address->getAddressLine2()) {
-                $addressNode->appendChild($xml->createElement('AddressLine', $this->address->getAddressLine2()));
-            }
-            if ($this->address->getAddressLine3()) {
-                $addressNode->appendChild($xml->createElement('AddressLine', $this->address->getAddressLine3()));
-            }
-            if ($this->address->getStateProvinceCode()) {
-                $addressNode->appendChild($xml->createElement('PoliticalDivision1',
-                    $this->address->getStateProvinceCode()));
-            }
-            if ($this->address->getCity()) {
-                $addressNode->appendChild($xml->createElement('PoliticalDivision2', $this->address->getCity()));
-            }
-            if ($this->address->getCountryCode()) {
-                $addressNode->appendChild($xml->createElement('CountryCode', $this->address->getCountryCode()));
-            }
-            if ($this->address->getPostalCode()) {
-                $addressNode->appendChild($xml->createElement('PostcodePrimaryLow', $this->address->getPostalCode()));
-            }
         }
 
         return $xml->saveXML();
